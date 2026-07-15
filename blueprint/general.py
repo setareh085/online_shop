ab0688a93d0c709f5f59cda45a5065e40c120f9c
Setareh <setareh.085h@gmail.com>
2026-07-15 09:21:17 +0330

    main and about added

--- blueprint/general.py
+++ blueprint/general.py
@@ -1,10 +1,13 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from blueprint.admin import products
+from models.product import Product
 
 app = Blueprint("general", __name__)
 @app.route('/')
 def main():  # put application's code here
-    return 'Hello World!'
+    products = Product.query.all()
+    return render_template("main.html", products=products)
 
 @app.route('/about')
 def about():  # put application's code here
-    return 'about page'
+    return render_template("about.html")
