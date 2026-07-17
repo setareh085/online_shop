9c484ce2d36d12ee3696878a32785ab16e100724
Setareh <setareh.085h@gmail.com>
2026-07-17 09:52:40 +0330

    login and register user added

--- blueprint/user.py
+++ blueprint/user.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_user
 from passlib.hash import sha256_crypt
 from extentions import db
@@ -10,14 +10,29 @@ def login():  # put application's code here
         return render_template("user/login.html")
     else:
         register = request.form.get("register", None)
-        username = request.form['username']
-        password = request.form['password']
-        phone = request.form['phone']
-        address = request.form['address']
+        username = request.form.get('username', None)
+        password = request.form.get('username', None)
+        phone = request.form.get('username', None)
+        address = request.form.get('username', None)
         if register != None:
+            user = User.query.filter(User.username == username).first()
+            if user == None:
+                flash("نام کاربری دیگری انتخاب کنید.")
+                return redirect(url_for("user.login"))
             user = User(username=username, password=sha256_crypt.encrypt(password), phone=phone, address=address)
             db.session.add(user)
             db.session.commit()
             login_user(user)
             return redirect("/user/dashboard")
+        else:
+            user = User.query.filter(User.username == username).first()
+            if user == None:
+                flash("نام کاربری یا رمز عبور اشتباه است.")
+                return redirect(url_for("user.login"))
+            if sha256_crypt.verify(password, user.password):
+                login_user(user)
+                return redirect("/user/dashboard")
+            else:
+                flash("نام کاربری یا رمز عبور اشتباه است.")
+                return redirect(url_for("user.login"))
         return "done"
